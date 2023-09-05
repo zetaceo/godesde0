5fe8a025dbcf97c877cc5cc675f5887604dab6c1
Zetaceo <setzer.vampire@gmail.com>
2023-09-05 14:29:43 -0300

    Subo el ejercicio01

---
+++ ejercicios/ejercicio01.go
@@ -0,0 +1,21 @@
+package ejercicios
+
+import (
+	"strconv"
+)
+
+func Ejercicio(texto string) (int, string) {
+	valor, err := strconv.Atoi(texto)
+	if err != nil {
+		return 0, "Hubo un error"
+	}
+	var texto_resultado string
+
+	if valor > 100 {
+		texto_resultado = "El valor es mayor a 100"
+	} else {
+		texto_resultado = "El valor es menor a 100"
+	}
+
+	return valor, texto_resultado
+}
--- main.go
+++ main.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 
-	"github.com/zetaceo/godesde0/variables"
+	"github.com/zetaceo/godesde0/ejercicios"
 )
 
 func main() {
-	estado, texto := variables.ConviertoaTexto(1588)
+	/*estado, texto := variables.ConviertoaTexto(1588)
 	fmt.Println(estado)
+	fmt.Println(texto) */
+
+	if os := runtime.GOOS; os == "Linux." || os == "OS X." {
+		fmt.Println("Unix based!", os)
+	} else {
+		fmt.Println("Un guindos", os)
+	}
+
+	numero, texto := ejercicios.Ejercicio("150")
+
+	fmt.Println(numero)
 	fmt.Println(texto)
 }
